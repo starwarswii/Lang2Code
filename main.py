5e2c2409762e629e274af33cc2b787ca1b62de30
starwarswii <starwarswii@gmail.com>
2017-11-11 15:37:36 -0500

    base commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+m = sr.Microphone()
+
+try:
+    print("be silent")
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+    print("minimum energy threshold: ".format(r.energy_threshold))
+    
+    while True:
+        try:
+        
+            print("listening")
+            with m as source:
+                audio = r.listen(source)
+            print("recognizing")
+            
+            value = r.recognize_google(audio)
+
+            print(value)
+             
+        except sr.UnknownValueError:
+            print("didn't hear you")
+        except sr.RequestError as e:
+            print("request error: {}".format(e))
+            
+except KeyboardInterrupt:
+    pass
